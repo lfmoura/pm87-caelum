dff98682a112f08dd7f3c6cba1093e402f5cc134
agil1901 <agil1901@caelum57-10.(none)>
2011-04-11 22:41:39 -0300

    teste

--- exemploGit.java
+++ exemploGit.java
@@ -1,5 +1,5 @@
 public class exemploGit{
 	public static void main(String[] args){
-		System.out.println("Exemplo teste de uso do git 3");
+		System.out.println("Exemplo teste de uso do git alterado");
 	}
 }
